63648c2ef4f769c033c176254c3962ca6e25bcaa
Pedro Soares <pmsoares@gmail.com>
2023-04-13 12:25:31 +0100

    Add project files.

---
+++ EISNT_UFCD0815.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EISNT_UFCD0815.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
